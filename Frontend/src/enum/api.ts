3d70c85018e3383d15d99389a4549c237fd08429
kevin083177 <kevin083177@gmail.com>
2025-01-08 22:55:38 +0800

    Update for .env

--- Frontend/src/enum/api.ts
+++ Frontend/src/enum/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 const API_VERSION = '/api/v1';
 
 const AUTH_URL = `${API_BASE_URL}${API_VERSION}/auth`;
@@ -9,7 +9,7 @@ export const auth_api = {
     login: `${AUTH_URL}/login`,
     register: `${AUTH_URL}/register`,
     logout: `${AUTH_URL}/logout`
-} as const;
+};
 
 export const user_api = {
     getAllPoints: `${USER_URL}/getAllUserPoints`,
@@ -17,10 +17,10 @@ export const user_api = {
     updatePoints: `${USER_URL}/updatePoints`,
     deleteUser: `${USER_URL}/deleteByUserId`,
     updatePassword: `${USER_URL}/updatePassword`
-} as const;
+};
 
 export const admin_api = {
     getAllUser: `${ADMIN_URL}/getAllUser`,
     resetUserPoints: `${ADMIN_URL}/resetUserPoints`,
     revokeUser: `${ADMIN_URL}/revokeUser`,
-} as const;
+};
